059895f68d71591e4dabba3091db50a176d82d92
Santhosh-R1 <santhoshrajan817@gmail.com>
2026-02-23 13:16:04 +0530

    fix that

--- client/src/NavigationHandler.jsx
+++ client/src/NavigationHandler.jsx
@@ -6,6 +6,13 @@ const NavigationHandler = () => {
   const location = useLocation();
 
   useEffect(() => {
+    const targetPaths = ['/', '/about', '/archive'];
+    if (targetPaths.includes(location.pathname)) {
+      window.scrollTo(0, 0);
+    }
+  }, [location.pathname]);
+
+  useEffect(() => {
     const unblock = window.addEventListener('popstate', () => {
       navigate(location.pathname, { replace: true });
     });
